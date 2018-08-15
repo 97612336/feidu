530350d50135c501ba6a9c09b132b2f81daaa8b2
wangkun <wangkun@autoforce.net>
2018-08-15 11:11:43 +0800

    增加测试文件

--- handlers/novel_api/one_api.go
+++ handlers/novel_api/one_api.go
@@ -1,11 +1,18 @@
 package novel_api
 
-import "net/http"
+import (
+	"net/http"
+	"feidu/util"
+)
 
 func Get_some_book_name(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(1024 * 1024 * 3)
 	if r.Method == "GET" {
+		var data = make(map[string]interface{})
 		//获取一组随机数的数组,数组容量为20个
-
+		md5_str := util.Get_md5str("wksgdsg")
+		data["code"] = 200
+		data["msg"] = md5_str
+		util.Return_json(w,data)
 	}
 }
--- handlers/urls.go
+++ handlers/urls.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"net/http"
 	"feidu/handlers/imgapi"
+	"feidu/handlers/novel_api"
 )
 
 func MyUrls() {
 	http.HandleFunc("/img/receiver", imgapi.Receive_img)
+	http.HandleFunc("/test/one", novel_api.Get_some_book_name)
 }
