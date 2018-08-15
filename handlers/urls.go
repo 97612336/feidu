03c810af0fecd6da0766bc924a5e5caec178e53a
wangkun <wangkun@autoforce.net>
2018-08-15 11:57:36 +0800

    删除多余的文件

--- handlers/urls.go
+++ handlers/urls.go
@@ -2,11 +2,9 @@ package handlers
 
 import (
 	"net/http"
-	"feidu/handlers/imgapi"
 	"feidu/handlers/novel_api"
 )
 
 func MyUrls() {
-	http.HandleFunc("/img/receiver", imgapi.Receive_img)
 	http.HandleFunc("/test/one", novel_api.Get_some_book_name)
 }
