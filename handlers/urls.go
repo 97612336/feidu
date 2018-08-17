3936f16576f63ad975e5e0a766f232cd4798b905
wangkun <wangkun@autoforce.net>
2018-08-17 09:52:49 +0800

    更改url链接

--- handlers/urls.go
+++ handlers/urls.go
@@ -6,7 +6,7 @@ import (
 )
 
 func MyUrls() {
-	http.HandleFunc("/test/one", novel_api.Get_some_book_name)
-	http.HandleFunc("/test/two", novel_api.Get_chapter_name_by_book_id)
-	http.HandleFunc("/test/three", novel_api.Get_one_chapter_by_id)
+	http.HandleFunc("/v1/get_random_book", novel_api.Get_some_book_name)
+	http.HandleFunc("/v1/get_chapter_name", novel_api.Get_chapter_name_by_book_id)
+	http.HandleFunc("/v1/get_text", novel_api.Get_one_chapter_by_id)
 }
