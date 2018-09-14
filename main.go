31ec6c97ba4c51fab883f23b14c6733589480c18
wangkun <wangkun@autoforce.net>
2018-09-14 20:35:53 +0800

    增加启动项目时的端口指定

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"net/http"
 	"feidu/handlers"
-	_ "github.com/go-sql-driver/mysql"
 	"feidu/util"
+	"flag"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
 )
 
 func init() {
@@ -14,8 +16,13 @@ func init() {
 func main() {
 	//设置路由
 	handlers.MyUrls()
+	//设置端口号
+	var port string
+	flag.StringVar(&port,"port","8081","listen port")
+	flag.Parse()
+	fmt.Println(port)
 	//设置监听端口
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":"+port, nil)
 	//启动程序
 	util.CheckErr(err)
 
